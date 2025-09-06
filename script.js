44f019d9357cb58a9954d3e13ce28ceb314d22ee
Ionita Aurel Mihai <ionitaaurel32@gmail.com>
2025-09-06 19:33:01 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+
+let productImg = document.getElementById("productImg");
+let btns = document.querySelectorAll(".btn");
+ const images = [
+            "images/image1.png",
+            "images/image2.png",
+            "images/image3.png"
+];
+btns.forEach((btn, idx) => {
+    btn.onclick = function() {
+        productImg.src = images[idx];
+        btns.forEach(b => b.classList.remove("active"));
+        this.classList.add("active");
+    }
+});
