bc7503bfd4925fc91181ae4e9a2e3c1900d57601
Ionita Aurel Mihai <ionitaaurel32@gmail.com>
2025-09-07 15:56:12 +0300

    feat:am adaugat scriptul

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 
 let productImg = document.getElementById("productImg");
-let btns = document.querySelectorAll(".btn");
- const images = [
-            "images/image1.png",
-            "images/image2.png",
-            "images/image3.png"
+let btns = document.querySelectorAll(".btn"); // presupunem că butoanele au clasa 'btn'
+
+const images = [
+    "images/image1.png",
+    "images/image2.png",
+    "images/image3.png"
 ];
+
 btns.forEach((btn, idx) => {
     btn.onclick = function() {
         productImg.src = images[idx];
